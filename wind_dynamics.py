a4588e3b2ac94aef6b3affa9ef85c74bff183662
FADM ECNM CORY ANDREW HOFSTAD USN <96269532+FADM-DCMN-CORY-A-HOFSTAD-USN@users.noreply.github.com>
2026-06-07 00:18:20 -0700

    Update wind_dynamics.py

--- wind_dynamics.py
+++ wind_dynamics.py
@@ -1,7 +1,6 @@
 import numpy as np
 
 # --- PRIMARY ENGINE: [Model Name] ---
-import streamlit as st
 import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
@@ -12,6 +11,7 @@ import aviation_telemetry      # Data flow
 import aircraft_perf           # Performance calculations
 import sensor_thermodynamics   # Env data scaling
 import aerodynamic_matrix      # Lift/Drag logic
+import streamlit as st
 
 try:
     import cupy as np  # Attempt to use GPU-accelerated array math
